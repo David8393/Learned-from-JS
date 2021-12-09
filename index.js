e5abd9a51cc687d57e1dc2a46b8ca8e776cea7eb
David8393 <jo974586david@gmail.com>
2021-12-09 12:06:36 +0500

    test

--- index.js
+++ index.js
@@ -19,3 +19,33 @@ btn.onclick = function() {
   
 }
 
+const points = [40, 100, 1, 5, 25, 10];
+document.getElementById("demo5").innerHTML = points;  
+
+points.sort(function (a, b) {
+  return a - b
+});
+document.getElementById("demo6").innerHTML = points;
+
+const points1 = ['webbrainacademy',  'academy',  'g2'];
+document.getElementById("demo7").innerHTML = points1;  
+
+points1.sort(function (a, b) {
+  return a.length - b.length
+
+});
+document.getElementById("demo8").innerHTML = points1;
+
+// const points2 = ['webbrainacademy',  'academy',  'g2'];
+// document.getElementById("demo7").innerHTML = points2;  
+
+// points2.sort()
+// document.getElementById("demo8").innerHTML = points2;
+
+// arr['ABCabdiidinidnBnn@_--']
+// for (let i = 0; i < arr.length; i++) {
+// if (arr) {
+  
+// }
+// }
+// console.log(arr);
